903d7888e3d750ff94707d3ba352f11167daf0ee
Danielle Stone Awounang <stonetemfack@gmail.com>
2023-03-18 16:08:23 +0100

    envoie des elements du recycleview à l'écran

--- app/src/main/java/com/example/notes/Adapter/NoteAdapter.kt
+++ app/src/main/java/com/example/notes/Adapter/NoteAdapter.kt
@@ -1,31 +1,94 @@
 package com.example.notes.Adapter
 
 import android.content.Context
+import android.os.Build
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.annotation.RequiresApi
 import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.notes.Models.Note
 import com.example.notes.R
+import kotlin.random.Random
 
-class NoteAdapter(private val context: Context):RecyclerView.Adapter<NoteAdapter.noteViewHolder> {
+class NoteAdapter(private val context: Context, private val listener: NotesItemClickListener):RecyclerView.Adapter<NoteAdapter.noteViewHolder>() {
 
-    private val notesList:ArrayList<Note>()
-    private val fullList:ArrayList<Note>()
+    private val notesList=ArrayList<Note>()
+    private val fullList=ArrayList<Note>()
 
 
+            //lors de la creation du viewholder
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): noteViewHolder {
-        TODO("Not yet implemented")
+        return noteViewHolder(
+            LayoutInflater.from(context).inflate(R.layout.list_item, parent,false)
+        )
     }
 
+    // lors de l'appel du viewholder
+    @RequiresApi(Build.VERSION_CODES.M)
     override fun onBindViewHolder(holder: noteViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        val currentNote= notesList[position]
+        holder.title.text = currentNote.title
+        holder.title.isSelected= true
+
+        holder.note_tv.text= currentNote.note
+        holder.date.text=currentNote.date
+        holder.date.isSelected=true
+
+        holder.notes_layout.setCardBackgroundColor(holder.itemView.resources.getColor(randomColor(),null))
+
+        holder.notes_layout.setOnClickListener {
+            listener.onItemClicked(notesList[holder.adapterPosition])
+        }
+
+        holder.notes_layout.setOnLongClickListener {
+            listener.onLongitemClicked(notesList[holder.adapterPosition].holder.notes_layout)
+            true
+        }
+    }
+
+    fun updateList(newList:List<Note>){
+        fullList.clear()
+        fullList.addAll(newList)
+
+        notesList.clear()
+        notesList.addAll(fullList)
+        notifyDataSetChanged()
+    }
+
+    fun fillterList(search:String){
+        notesList.clear()
+
+        for(item in fullList){
+            if(item.title?.lowercase()?.contains(search.lowercase())==true||
+                item.note?.lowercase()?.contains(search.lowercase())==true){
+                notesList.add(item)
+            }
+        }
+        notifyDataSetChanged()
     }
 
     // recuperation du nombre de notes
     override fun getItemCount(): Int {
         return notesList.size
+    }
+
+    fun randomColor():Int{
+
+        val list = ArrayList<Int>()
+        list.add(R.color.NoteColor1)
+        list.add(R.color.NoteColor2)
+        list.add(R.color.NoteColor3)
+        list.add(R.color.NoteColor4)
+        list.add(R.color.NoteColor5)
+        list.add(R.color.NoteColor6)
+
+        val seed = System.currentTimeMillis().toInt()
+        val randomIndex = Random(seed).nextInt()
+
+        return  list[randomIndex]
     }
 
     // recuperation des elements de la liste
@@ -34,5 +97,11 @@ class NoteAdapter(private val context: Context):RecyclerView.Adapter<NoteAdapter
         val title = itemView.findViewById<TextView>(R.id.tv_title)
         val note_tv = itemView.findViewById<TextView>(R.id.tv_note)
         val date = itemView.findViewById<TextView>(R.id.tv_date)
+    }
+
+    interface NotesItemClickListener{
+
+        fun onItemClicked(note: Note)
+        fun onLongitemClicked(note: Note, cardView: CardView)
     }
 }
--- app/src/main/java/com/example/notes/MainActivity.kt
+++ app/src/main/java/com/example/notes/MainActivity.kt
@@ -1,11 +1,81 @@
 package com.example.notes
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.GridLayout.VERTICAL
+import android.widget.LinearLayout
+import android.widget.SearchView
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
+import com.example.notes.Adapter.NoteAdapter
+import com.example.notes.Database.NoteDatabase
+import com.example.notes.Models.Note
+import com.example.notes.Models.NoteViewModel
+import com.example.notes.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var biding:ActivityMainBinding
+    private lateinit var  database:NoteDatabase
+    lateinit var viewModel: NoteViewModel
+    lateinit var  adapter:NoteAdapter
+    lateinit var selectedNoted: Note
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        biding=ActivityMainBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_main)
+
+        //initialisatio du UI
+        UnitUI()
+
+        viewModel=ViewModelProvider(this,
+            ViewModelProvider.AndroidViewModelFactory.getInstance(application)).get(NoteViewModel::class.java)
+        viewModel.allNotes.observe(this){ list->
+            list?.let{
+                adapter.updateList(list)
+            }
+        }
+
+        database = NoteDatabase.getDataBase(this)
+
+    }
+
+    private fun UnitUI() {
+        biding.recyclerView.setHasFixedSize(true)
+        biding.recyclerView.layoutManager = StaggeredGridLayoutManager(
+            2,LinearLayout.VERTICAL )
+        adapter = NoteAdapter(this,this)
+        biding.recyclerView.adapter=adapter
+
+        val getContent = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){result->
+            if(result.resultCode== Activity.RESULT_OK){
+                val note = result.data?.getSerializableExtra("note") as? Note
+
+                if(note!=null){
+                    viewModel.insertNote(note)
+                }
+            }
+        }
+
+        biding.addNote.setOnClickListener{
+            val intent = Intent(this, AddNote::class.java)
+            getContent.launch(intent)
+        }
+
+        biding.searchView.setOnQueryTextListener(object:SearchView.OnQueryTextListener{
+            override fun onQueryTextSubmit(p0: String?): Boolean {
+                TODO("Not yet implemented")
+            }
+
+            override fun onQueryTextChange(p0: String?): Boolean {
+                TODO("Not yet implemented")
+            }
+        }
+
     }
 }
